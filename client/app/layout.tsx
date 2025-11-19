aa11f179e33fc438d1216017e1d75c76bf9b32a4
mathiasomar <mathiasomar@gmail.com>
2025-11-19 13:57:46 +0300

    Copy UI

---
+++ client/app/layout.tsx
@@ -0,0 +1,51 @@
+import type { Metadata } from "next";
+import { Inter, Playfair_Display } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "sonner";
+import Navbar from "@/components/navbar";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Suspense } from "react";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
+
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-playfair",
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "NajTrends Fashion Store",
+  description: "Your one-stop shop for the latest fashion trends.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <Suspense fallback={null}>
+      <ClerkProvider>
+        <html
+          lang="en"
+          style={{ overflowX: "hidden" }}
+          suppressHydrationWarning
+        >
+          <head></head>
+          <body
+            className={`${inter.variable} ${playfair.variable} font-sans antialiased`}
+          >
+            <Navbar />
+            {children}
+            <Toaster position="top-center" />
+          </body>
+        </html>
+      </ClerkProvider>
+    </Suspense>
+  );
+}
---
+++ client/app/test/page.tsx
@@ -0,0 +1,33 @@
+import { auth } from "@clerk/nextjs/server";
+
+const TestPage = async () => {
+  const { getToken } = await auth();
+  const token = await getToken();
+  const resProduct = await fetch("http://localhost:8000/test", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const dataProduct = await resProduct.json();
+
+  const resOrder = await fetch("http://localhost:8001/test", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const dataOrder = await resOrder.json();
+
+  const resPayment = await fetch("http://localhost:8002/test", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const dataPayment = await resPayment.json();
+
+  console.log(dataProduct);
+  console.log(dataOrder);
+  console.log(dataPayment);
+  return <div>TestPage</div>;
+};
+
+export default TestPage;
