c9874d032c1b9693302c0818a8061e60eccee107
mathiasomar <mathiasomar@gmail.com>
2025-11-19 14:15:02 +0300

    Initial: Create express backend

---
+++ backend/src/index.ts
@@ -0,0 +1,35 @@
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+import { shouldBeUser } from "./middleware/authMiddleware.js";
+
+const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:3002", "http://localhost:3003"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).json({
+    status: "ok",
+    uptime: process.uptime(),
+    timestamp: Date.now(),
+  });
+});
+
+app.get("/test", shouldBeUser, (req: Request, res: Response) => {
+  res.json({ message: "Product service authenticated", userId: req.userId });
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(err.message);
+  return res
+    .status(err.status || 500)
+    .json({ message: err.message || "Internal server error" });
+});
+
+app.listen(8000, () => console.log("Product service running on port 8000"));
